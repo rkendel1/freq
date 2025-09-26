3f96316bde09358780504755e537aad328d08f95
T448 <tatsuda4480712@gmail.com>
2025-09-26 11:09:12 +0000

    [add]devcontainerでclaude codeを使えるよう変更 #1
    拡張機能もいくつか追加

---
+++ .devcontainer/scripts/post-create.sh
@@ -0,0 +1,7 @@
+# claude-code, gemini-cli, lefthook をインストール
+npm install -g @anthropic-ai/claude-code@1.0.123 @google/gemini-cli@0.6.1 lefthook@1.13.4
+
+# CCPluginsをインストール
+curl -sSL https://raw.githubusercontent.com/brennercruvinel/CCPlugins/main/install.sh | bash
+
+lefthook install
