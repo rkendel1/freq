9d4af1d7a1cdd14bd996ecef6c91198b27974ff5
T448 <tatsuda4480712@gmail.com>
2025-09-26 11:29:29 +0000

    [add]tig,procpsを追加

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -1,3 +1,7 @@
+sudo apt-get update
+sudo apt-get install -y tig procps
+sudo apt-get clean
+
 # claude-code, gemini-cli, lefthook をインストール
 npm install -g @anthropic-ai/claude-code@1.0.123 @google/gemini-cli@0.6.1 lefthook@1.13.4
 
